21a91cfdc722120c7bd7421062b1b9798bc9cd86
deki90to <deki90to@gmail.com>
2021-03-20 17:51:50 +0100

    editing posts

--- app.py
+++ app.py
@@ -70,9 +70,25 @@ def friends():
 			db.session.add(new_friend)
 			db.session.commit()
 			return redirect('/friends')
-			
+
 		except:
 			return 'There was an error adding'
 	else:
 		friends = Friends.query.order_by(Friends.date)
 		return render_template('friends.html', title=title, friends=friends)
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+	update_friend = Friends.query.get_or_404(id)
+
+	if request.method == 'POST':
+		update_friend.name = request.form['name']
+
+		try:
+			db.session.commit()
+			return redirect('/friends')
+		except:
+			return 'There was a problem with updating name'
+	else:
+		return render_template('update.html', update_friend=update_friend)
